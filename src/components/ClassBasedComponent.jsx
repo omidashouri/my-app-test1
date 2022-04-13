be67f2bc05124ea6ab760884bb36c5d9d8a72414
omidashouri <omidashouri@gmail.com>
2022-04-13 14:49:33 +0430

    end 12 : omidashouri: 010124

--- src/components/ClassBasedComponent.jsx
+++ src/components/ClassBasedComponent.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 class ClassBasedComponent extends Component{
 
-    state = {
+/*    state = {
         count: 0,
         flag: false
     }
@@ -26,20 +26,75 @@ class ClassBasedComponent extends Component{
                 flag: true
             })
         }
+    }*/
+
+
+    state = {
+        formData :{
+            name : '',
+            age:0
+        },
+        finalFormData : {}
     }
+
+    handleNameChange =(event)=> {
+        const {value} = event.target;
+        this.setState(prevState=>({
+            formData : {
+                ...prevState.formData,
+                name : value
+            }
+        }))
+    }
+
+    handleAgeChange = (event)=> {
+        const {value} = event.target;
+        this.setState(prevState=>({
+            formData : {
+                ...prevState.formData,
+                age : value
+            }
+        }))
+    }
+
+    handleSubmit = (event)=>{
+        event.preventDefault()
+        const {formData} = this.state;
+        this.setState(prevState=>({
+            finalFormData : {
+                ...prevState.finalFormData,
+                ...formData
+            }
+        }))
+    }
+
+
 
     render() {
         return(
             <div>
                 {/*Class based component example*/}
 
-                <button onClick={this.handleCount }>Click button</button>
+                {/*<button onClick={this.handleCount }>Click button</button>
                 {
                     this.state.flag && <p>Flag is true</p>
+                }*/}
+
+                <form onSubmit={this.handleSubmit}>
+                    <input name="name" placeholder="name" type={"text"} value={this.state.formData.name} onChange={this.handleNameChange}/>
+                    <input name={"age"} placeholder={"age"} type={"number"} value={this.state.formData.age} onChange={this.handleAgeChange}/>
+                    <button type={"submit"} aria-label={"submit"}>Submit</button>
+                </form>
+                {
+                    this.state.finalFormData && Object.keys(this.state.finalFormData).length>0
                 }
+                final Data is {this.state.finalFormData.name} and {this.state.finalFormData.age}
+
             </div>
         )
     }
+
+
 }
 
 export default ClassBasedComponent;
