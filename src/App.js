4ad8228beed83b02da947cc4346b68a3cb561e42
omidashouri <omidashouri@gmail.com>
2022-04-13 09:33:34 +0430

    end 11 : omidashouri: 010124

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <ClassBasedComponent/>
-      <FunctionalBasedComponent/>
+      {/*<FunctionalBasedComponent/>*/}
 
     </div>
   );
--- src/components/ClassBasedComponent.jsx
+++ src/components/ClassBasedComponent.jsx
@@ -1,10 +1,42 @@
 import React, {Component} from "react";
 
 class ClassBasedComponent extends Component{
+
+    state = {
+        count: 0,
+        flag: false
+    }
+
+    handleCount = ()=>{
+        const {count} = this.state;
+        this.setState({
+            count: count+1
+        },()=>{
+            console.log(count)
+        })
+    }
+
+    componentDidMount(prevProps, prevState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevState && prevState.count !== this.state.count && this.state.count === 10){
+            this.setState({
+                flag: true
+            })
+        }
+    }
+
     render() {
         return(
             <div>
-                Class based component example
+                {/*Class based component example*/}
+
+                <button onClick={this.handleCount }>Click button</button>
+                {
+                    this.state.flag && <p>Flag is true</p>
+                }
             </div>
         )
     }
