9c6a4e185231aac0fc8d9fcd453b937fe1edc1d7
omidashouri <omidashouri@gmail.com>
2022-04-24 11:22:42 +0430

    router 2022-04-21_13-52-17 : omidashouri: 010204

--- src/index.js
+++ src/index.js
@@ -3,20 +3,26 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from "./11/router/AppRouter";
+import App2 from "./App2";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+ReactDOM.createRoot(
+    document.getElementById('root')
+).render(<AppRouter/>);
+
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App2 />*/}
-
-      <AppRouter />
+    <App2 />
 
   </React.StrictMode>
 
     //40:14
     // <Users/>
 
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
