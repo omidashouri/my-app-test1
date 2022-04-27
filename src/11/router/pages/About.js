199bca0837b46f1cf08a6ab37ab14eef4ec88bf4
omidashouri <omidashouri@gmail.com>
2022-04-27 08:52:50 +0430

    11 finished : omidashouri: 010207

--- src/11/router/pages/About.js
+++ src/11/router/pages/About.js
@@ -1,5 +1,19 @@
+import {useChangeInput} from "../useChangeInput";
+
+
 const About = ()=>{
-    return <h1>About</h1>
+    const [name, setName] = useChangeInput('');
+    console.log(name);
+
+    return (
+        <>
+        <h1>About</h1>
+            <input type={'text'} value={name}
+                   // onChange={(e)=>{setName(e.target.value)}}
+            onChange={setName}
+            />
+        </>
+    )
 }
 
 export default About;
